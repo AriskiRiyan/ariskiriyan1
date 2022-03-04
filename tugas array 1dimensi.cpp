fc338aed6ecc2a6dceaada74630b6da6c2d3e65c
AriskiRiyan <96316832+AriskiRiyan@users.noreply.github.com>
2022-03-04 19:58:39 +0700

    Add files via upload

---
+++ tugas array 1dimensi.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	
+	printf("\nNIM		:07.1.2021.0036 ");
+	
+	printf("\nNAMA		:ARISKI RIYAN TRI ADMAJA ");
+	
+	printf("\nJURUSAN		:Teknik Informatika ");
+
+	return 0;
+}
